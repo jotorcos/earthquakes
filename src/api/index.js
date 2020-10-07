f47def602af40df088acfa9f09d0e702b48758ff
Josep Tormo <jotorcos@Icarus.local>
2020-10-07 10:27:36 +0200

    Refactor to baseUrl in api

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,17 @@
 export default {
+  baseUrl: 'https://earthquake.usgs.gov/fdsnws/event/1/',
+
   async getEarthquakesByDate(dateStart, dateEnd) {
     const response = await fetch(
-      `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${dateStart.toISOString()}&endtime=${dateEnd.toISOString()}`
+      `${
+        this.baseUrl
+      }query?format=geojson&starttime=${dateStart.toISOString()}&endtime=${dateEnd.toISOString()}`
     )
     return await response.json()
   },
   async getEarthquakeById(id) {
     const response = await fetch(
-      `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventid=${id}`
+      `${this.baseUrl}query?format=geojson&eventid=${id}`
     )
     return await response.json()
   }
